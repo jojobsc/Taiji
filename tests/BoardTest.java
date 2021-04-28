4f6ac1c592136545e56a00a81b126ed284a5e086
viktoraleksiev <82540051+viktoraleksiev@users.noreply.github.com>
2021-04-28 11:04:28 +0200

    Add files via upload

--- tests/BoardTest.java
+++ tests/BoardTest.java
@@ -11,8 +11,26 @@ class BoardTest {
         String testBoard = "---wbbw/-----b-/-----w-/b------/wb-----/ww-----/bwb----";
 
         Board b = new Board(testBoard);
-        //System.out.print(b);
-
-        System.out.println("BITS moves = " + b.moveGenerator().size());
+        //System.out.println("moves = " + b.moveGenerator().size());
     }
+
+    @Test
+    void moveGenerator() {
+
+    }
+
+    @Test
+    void doMove() {
+        String testBoard = "---wbbw/-----b-/-----w-/b------/wb-----/ww-----/bwb----";
+
+        Board b = new Board(testBoard);
+        b.moveGenerator();
+        System.out.println(b);
+        System.out.println(b.getMoves().get(10));
+        b.doMove(b.getMoves().get(10));
+        System.out.println(b);
+
+
+    }
+
 }
