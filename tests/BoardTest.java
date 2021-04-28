9941f80fe73496f6927c31a176f14d0e5f34f860
jojobsc <83354782+jojobsc@users.noreply.github.com>
2021-04-28 17:32:15 +0200

    Add files via upload

--- tests/BoardTest.java
+++ tests/BoardTest.java
@@ -1,36 +1,41 @@
+package tests;
+
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.zip.DataFormatException;
+import taiji.Board;
+
+import static org.junit.Assert.assertEquals;
+
+
 
 class BoardTest {
-    @Test
-    void zuggenerator() {
-
-        String testBoard = "---wbbw/-----b-/-----w-/b------/wb-----/ww-----/bwb----";
-
-        Board b = new Board(testBoard);
-        //System.out.println("moves = " + b.moveGenerator().size());
-    }
+   
 
     @Test
     void moveGenerator() {
-
+    	String testBoard = "wb-bw-w/--wb-wb/wb--wb-/-wb-b-w/bw-wbwb/bwb--b-/w----bw";
+    	Board board = new Board(testBoard);
+    	String testBoard1 = "wb-bw-w/-----wb/wb--wb-/----b--/---wb--/b------/w----bw";
+    	Board board1 = new Board(testBoard1);
+    	assertEquals(18, board.moveGenerator().size());
+    	assertEquals(78, board1.moveGenerator().size());
+    	
+    	String testBoard2 = "w--w--w-w/b--b--b-b/-----w---/-----b---/-wb-w--wb/----b----/---------/w-wb-w--w/b----b--b";
+    	Board board2 = new Board(testBoard2);
+    	String testBoard3 = "wb-------bw/-----wb----/--------w--/-wb-w-w-b-w/----b-b---b/--w------w-/--b------b-/-bw--wb-w--/--------b--/-----w-----/wb---b---bw";
+    	Board board3 = new Board(testBoard3);
+    	assertEquals(152, board2.moveGenerator().size());
+    	assertEquals(248, board3.moveGenerator().size());
     }
 
+  
     @Test
-    void doMove() {
-        String testBoard = "---wbbw/-----b-/-----w-/b------/wb-----/ww-----/bwb----";
-
-        Board b = new Board(testBoard);
-        b.moveGenerator();
-        System.out.println(b);
-        System.out.println(b.getMoves().get(10));
-        b.doMove(b.getMoves().get(10));
-        System.out.println(b);
-
-
+    void toStringTest() {
+    	String testBoard = "---wbbw/-----b-/-----w-/b------/wb-----/ww-----/bwb----";
+    	Board board = new Board(testBoard);
+    	System.out.println(board.toString());
+    	
+    	
     }
 
 }
